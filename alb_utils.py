1623dfc8bcac362af454cfc6ebfdd620ccafc560
albertosnake <alberto_algon@hotmail.com>
2019-11-01 16:34:23 +0100

    Update alb_utils.py

--- alb_utils.py
+++ alb_utils.py
@@ -4,8 +4,9 @@ Created on Fri Nov  1 16:18:52 2019
 
 @author: Utilisateur
 """
+import os
 
 def alb_collab_to_pdf(ipynb_name): 
-  !apt-get install texlive texlive-xetex texlive-latex-extra pandoc
-  !pip install pypandoc
-  !jupyter nbconvert --to PDF "$ipynb_name"
+  os.system('apt-get install texlive texlive-xetex texlive-latex-extra pandoc')
+  os.system('pip install pypandoc')
+  os.system('jupyter nbconvert --to PDF'+ipynb_name)
