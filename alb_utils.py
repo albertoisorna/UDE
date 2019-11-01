10e23f1d164ef04486208d6a9a9e85c9d4185eea
albertosnake <alberto_algon@hotmail.com>
2019-11-01 16:52:56 +0100

    Update alb_utils.py

--- alb_utils.py
+++ alb_utils.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  1 16:18:52 2019
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-@author: Utilisateur
-"""
-import os
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
 
-def alb_collab_to_pdf(ipynb_name): 
+def alb_collab_to_pdf(filename): 
   os.system('apt-get install texlive texlive-xetex texlive-latex-extra pandoc')
   os.system('pip install pypandoc')
-  os.system('jupyter nbconvert --to PDF'+ipynb_name)
+  os.system('jupyter nbconvert --to PDF'+filename)
